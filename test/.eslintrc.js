cd23bad325efd4c77d3af53ed046ffb65fb3a075
Dieter Luypaert <dieterluypaert@gmail.com>
2017-01-03 18:56:04 +0100

    bumped dependencies, added precommit/prepush hooks, implemented own util where possible

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -3,9 +3,14 @@ module.exports = {
   extends: [
     'muriki',
     'muriki/env/test',
-    'muriki/plugin/jasmine',
+    'muriki/plugins/jasmine',
     'muriki/es/2015',
     'muriki/es/modules',
   ],
   parser: 'babel-eslint',
+  rules: {
+    'import/no-extraneous-dependencies': [2, {
+      devDependencies: ['**/*.test.js'],
+    }],
+  },
 };
