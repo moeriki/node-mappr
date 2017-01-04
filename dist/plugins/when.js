89d03a06ae010ad9527577b7c14ebfa447ac6d8a
Dieter Luypaert <dieterluypaert@gmail.com>
2017-01-04 20:20:17 +0100

    fix when implementation

--- dist/plugins/when.js
+++ dist/plugins/when.js
@@ -3,6 +3,9 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
 // private functions
 
 var isFunction = function isFunction(func) {
@@ -25,30 +28,52 @@ var when = exports.when = function when(mappr) {
       cases[_key - 1] = arguments[_key];
     }
 
-    var internalWhen = function internalWhen(pojo) {
-      var condition = mappr(mapper)(pojo);
+    var internalWhen = function internalWhen(pojo, parentCondition) {
+      var currentMapper = mapper;
+      if (parentCondition && typeof mapper !== 'function') {
+        currentMapper = function currentMapper(value) {
+          return parentCondition === value;
+        };
+      }
 
-      if (cases.length > 2 || cases.some(isInternalWhen)) {
-        // treat as switch case
-        var defaultCase = cases.find(isDefaultCase);
-        var match = null;
+      var condition = mappr(currentMapper)(parentCondition || pojo);
 
-        cases.filter(isInternalWhen).find(function (internalWhenCase) {
-          return match = internalWhenCase(condition);
-        });
+      if (cases.some(isInternalWhen)) {
+        var _ret = function () {
+          // treat as switch case
+          var match = void 0;
 
-        if (!match && defaultCase) {
-          match = defaultCase(condition);
-        }
+          cases.filter(isInternalWhen).find(function (internalWhenCase) {
+            match = internalWhenCase(pojo, condition);
+            return match;
+          });
 
-        return match;
+          if (match) {
+            return {
+              v: match
+            };
+          }
+
+          var defaultCase = cases.find(isDefaultCase);
+          if (!defaultCase) {
+            return {
+              v: match
+            };
+          }
+
+          return {
+            v: defaultCase(pojo)
+          };
+        }();
+
+        if ((typeof _ret === 'undefined' ? 'undefined' : _typeof(_ret)) === "object") return _ret.v;
       }
 
       // treat as if / else statement
       if (condition && cases[0]) {
-        return cases[0](condition);
+        return cases[0](pojo);
       } else if (!condition && cases[1]) {
-        return cases[1](condition);
+        return cases[1](pojo);
       }
 
       return undefined;
--- test/plugins/when.test.js
+++ test/plugins/when.test.js
@@ -3,6 +3,8 @@
 
 // modules
 
+import { get } from 'lodash/fp';
+
 import mappr from '../../lib';
 
 // private functions
@@ -11,26 +13,51 @@ const gt = (value1) => (value2) => value2 > value1;
 
 // tests
 
-it('should return if case', () => {
-  const mapper = mappr.when(gt(18), () => 'adult', () => 'minor');
-  expect(mapper(30)).toBe('adult');
+describe('if', () => {
+
+  it('should return if case', () => {
+    const mapper = mappr.when(gt(18), () => 'adult', () => 'minor');
+    expect(mapper(30)).toBe('adult');
+  });
+
+  it('should return else case', () => {
+    const mapper = mappr.when(gt(18), () => 'adult', () => 'minor');
+    expect(mapper(10)).toBe('minor');
+  });
+
+  it('should return undefined', () => {
+    const mapper = mappr.when(() => true);
+    expect(mapper('anything')).toBe(undefined);
+  });
+
 });
 
-it('should return else case', () => {
-  const mapper = mappr.when(gt(18), () => 'adult', () => 'minor');
-  expect(mapper(10)).toBe('minor');
-});
+describe('switch', () => {
 
-it('should return no if-else case');
+  it('should run if case with pojo', () => {
+    const mapper = mappr.when('isToggle', get('key1'), get('key2'));
+    expect(mapper({ isToggle: true, key1: 'value1', key2: 'value2' })).toBe('value1');
+    expect(mapper({ isToggle: false, key1: 'value1', key2: 'value2' })).toBe('value2');
+  });
 
-it('should when then', () => {
-  const mapper = mappr.when(
-    'age',
-    mappr.when(gt(59), () => 'senior'),
-    mappr.when(gt(18), () => 'adult'),
-    () => 'minor',
-  );
-  expect(mapper({ age: 10 })).toEqual('minor');
-  expect(mapper({ age: 30 })).toEqual('adult');
-  expect(mapper({ age: 60 })).toEqual('senior');
+  it('should run cases with condition result', () => {
+    const mapper = mappr.when(
+      'getKey',
+      mappr.when('ONE', get('key1')),
+      mappr.when('TWO', get('key2')),
+      get('key3'),
+    );
+    expect(mapper({ getKey: 'ONE', key1: 'value1' })).toBe('value1');
+    expect(mapper({ getKey: 'TWO', key2: 'value2' })).toBe('value2');
+    expect(mapper({ getKey: null, key3: 'value3' })).toBe('value3');
+  });
+
+  it('should return undefined if not match', () => {
+    const mapper = mappr.when(
+      'getKey',
+      mappr.when('ONE', get('key1')),
+    );
+    expect(mapper({})).toBe(undefined);
+  });
+
 });
